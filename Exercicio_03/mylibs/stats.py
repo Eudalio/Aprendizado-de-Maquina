9d9e8ae123a06897461ded01b069c225ab30085f
eudalio <tonnydallios@gmail.com>
2018-03-25 09:49:07 -0300

    modified .gitignore and create function var in mylibs

--- Exercicio_03/mylibs/stats.py
+++ Exercicio_03/mylibs/stats.py
@@ -1,4 +1,14 @@
 import numpy as np
 
-def mean(X):
-    return (np.sum(X)/len(X))
+def mean(x):
+    return (np.sum(x)/len(x))
+
+def var(y):
+    media = mean(y)
+    soma = 0;
+    var = 0;
+    for item in y:
+        soma += ((item - media)**2)
+    var = soma/len(y)
+    return var;
+    
