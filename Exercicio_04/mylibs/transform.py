c44f422acd12409913ab6f1832b962cc72bc5b26
eudalio <tonnydallios@gmail.com>
2018-04-01 14:08:57 -0300

    mylibs library transform module finalized

---
+++ Exercicio_04/mylibs/transform.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def normalize(x):
+    x_norm = x.copy(x)
+    _cols = x.shape[1]
+    for i in range(n_cols):
+        x_norm[:, i] = (x[:, i] - np.min(x[:, i])) / (np.max(x[:, i]) - np.min(x[:, i]))
+    return x_norm
+
+def standardization(x):
+    x_std = np.copy(x)
+    n_cols = x.shape[1]
+    for i in range(n_cols):
+        x_std[:, i] = (x[:, i] - np.mean(x[:, i])) / np.std(x[:, i])
+    return x_std
