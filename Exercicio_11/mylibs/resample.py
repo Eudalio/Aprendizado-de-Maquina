f0c996d3454cbe17aa82445bb3722f92c8a1411c
eudalio <tonnydallios@gmail.com>
2018-06-19 04:11:26 -0300

    List 11 - done

---
+++ Exercicio_11/mylibs/resample.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+
+def split_train_test(n_elem, perc_train, seed):
+    total = [ i for i in range(n_elem)]
+    elem_train = math.floor(perc_train*n_elem) #numero de elementos da funcao de treino
+    np.random.seed(seed)
+    np.random.shuffle(total)
+    idx_train = total[0:elem_train]
+    idx_test = total[elem_train:]
+    return idx_train, idx_test
+
+def split_k_fold(n_elem, n_splits, shuffle, seed):
